5d33f498af0646b33fc89354166023352dae7af5
kotolin-sub <tonuma777@gmail.com>
2024-06-05 13:15:24 +0900

    Add files via upload

---
+++ EaseEdit.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using YukkuriMovieMaker.Commons;
+using YukkuriMovieMaker.Controls;
+using YukkuriMovieMaker.Exo;
+using YukkuriMovieMaker.Player.Video;
+using YukkuriMovieMaker.Plugin.Effects;
+
+namespace YMMCustomEaseK
+{
+    [VideoEffect("カスタムイージングK", new[] { "" }, new string[] { })]
+    internal class EaseEdit : VideoEffectBase
+    {
+
+
+        public override string Label => "カスタムイージングK";
+        /*[Display(Name = "X1", Description = "X1")]
+        [AnimationSlider("F0", "", -100, 100)]
+        public Animation X1 { get; } = new Animation(0, -10000, 10000);
+
+        [Display(Name = "X2", Description = "X2")]
+        [AnimationSlider("F0", "", -100, 100)]
+        public Animation X2 { get; } = new Animation(0, -10000, 10000);*/
+
+        [Display(Name = "Func", Description = "Func")]
+        [TextEditor(AcceptsReturn = true)]
+        public string Text { get => text; set => Set(ref text, value); }
+        string text = string.Empty;
+
+        [Display(Name = "error", Description = "error")]
+        [TextEditor(AcceptsReturn = true)]
+        public string Error { get => text; set => Set(ref text, value); }
+        string error = string.Empty;
+
+        //対象を設定できるようにしてた時の残骸
+        //かと思ったけど普通にそのまま
+        [Display(Name = "対象")]
+        [EnumComboBox]
+        public Subject Enum { get => subject; set => Set(ref subject, value); }
+        Subject subject = Subject.X;
+
+        [Display(GroupName = "グループ名", Name = "(関係ないよ)こいつを消す方法を教えてくれnull入れると堕ちるんだ", Description = "項目の説明")]
+        [AnimationSlider("F0", "%", 0, 100)]
+        public Animation Animation { get; } = new Animation(100, 0, 100);
+
+        //よく見てないのでサンプルのまま
+        public override IEnumerable<string> CreateExoVideoFilters(int keyFrameIndex, ExoOutputDescription exoOutputDescription)
+        {
+            return Enumerable.Empty<string>();
+        }
+
+        public override IVideoEffectProcessor CreateVideoEffect(IGraphicsDevicesAndContext devices)
+        {
+            return new EaseEditP(this);
+        }
+
+        protected override IEnumerable<IAnimatable> GetAnimatables() => new[] { Animation };
+
+        public enum Subject
+        {
+            [Display(Name = "X")]
+            X,
+            [Display(Name = "Y")]
+            Y,
+            [Display(Name = "拡大率")]
+            Z,
+            [Display(Name ="回転角(なんかよくわかんないので未実装)")]
+            R,
+        }
+    }
+}
---
+++ EaseEditP.cs
@@ -0,0 +1,102 @@
+﻿using MoonSharp.Interpreter;
+using Vortice.Direct2D1;
+using YukkuriMovieMaker.Player.Video;
+
+namespace YMMCustomEaseK
+{
+    internal class EaseEditP : IVideoEffectProcessor
+    {
+        readonly EaseEdit item;
+        ID2D1Image? input;
+
+        Script script = new Script();
+
+        public ID2D1Image Output => input ?? throw new NullReferenceException(nameof(input) + "is null");
+
+        public EaseEditP(EaseEdit item)
+        {
+            this.item = item;
+        }
+
+        [Obsolete]
+        public DrawDescription Update(EffectDescription effectDescription)
+        {
+            var frame = effectDescription.ItemPosition.Frame;
+            var length = effectDescription.ItemDuration.Frame;
+            var fps = effectDescription.FPS;
+
+            var subject = item.Enum;
+            var text = item.Text;
+
+
+            //
+            script.Globals["_FRAME"] = frame;
+            script.Globals["_LENGTH"] = length;
+            script.Globals["_FPS"] = fps;
+
+            /*script.Globals["X1"] = x1;
+            script.Globals["X2"] = x2;*/
+
+
+            try { script.DoString(text); } catch (Exception ex) { item.Error = Convert.ToString(ex); };
+
+            var ReT = script.Globals["ReT"];
+
+            /*
+            double t = (double)frame / (double)length;
+            double easeT = Math.Pow((1 - Math.Cos(t * Math.PI)) / 2, len);
+            var ret = x1 + (x2 - x1) * easeT;
+
+            var X = effectDescription.DrawDescription.DrawPoint.X;
+            var Y = effectDescription.DrawDescription.DrawPoint.Y;
+            */
+
+            switch (Convert.ToString(subject))
+            {
+
+                case "X":
+                    return new DrawDescription(
+                    effectDescription.DrawDescription,
+                    draw: new System.Numerics.Vector2(
+                        effectDescription.DrawDescription.DrawPoint.X + Convert.ToSingle(ReT),
+                        effectDescription.DrawDescription.DrawPoint.Y
+                        ));
+                case "Y":
+                    return new DrawDescription(
+                    effectDescription.DrawDescription,
+                    draw: new System.Numerics.Vector2(
+                        effectDescription.DrawDescription.DrawPoint.X,
+                        effectDescription.DrawDescription.DrawPoint.Y + Convert.ToSingle(ReT)
+                        ));
+                case "R"://TODO:実装
+                    return effectDescription.DrawDescription;/*new DrawDescription(
+                        effectDescription.DrawDescription,
+                        rotation: new System.Numerics.Vector3(
+                            effectDescription.DrawDescription.Rotation.X + Convert.ToSingle(ReT),
+                            effectDescription.DrawDescription.Rotation.Y + Convert.ToSingle(ReT),
+                            effectDescription.DrawDescription.Rotation.Z + Convert.ToSingle(ReT)
+                            ));*/
+                case "Z":
+                    return new DrawDescription(
+                        effectDescription.DrawDescription,
+                        zoom: new System.Numerics.Vector2(
+                            effectDescription.DrawDescription.Zoom.X + Convert.ToSingle(ReT)
+                            ));
+                default: return effectDescription.DrawDescription;
+            }
+        }
+        public void ClearInput()
+        {
+            input = null;
+        }
+        public void SetInput(ID2D1Image input)
+        {
+            this.input = input;
+        }
+
+        public void Dispose()
+        {
+
+        }
+    }
+}
