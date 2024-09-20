5896ea4a7a6ae2d0762bc3ccc4c405b97d0f94a4
kotolin-sub <tonuma777@gmail.com>
2024-09-20 20:04:46 +0900

    Update EaseEditP.cs

--- EaseEditP.cs
+++ EaseEditP.cs
@@ -1,8 +1,7 @@
-﻿using System;
+using System;
 using System.IO;
 using System.Runtime.InteropServices;
 using Vortice.Direct2D1;
-using YMM4GlobalVar;
 using YukkuriMovieMaker.Commons;
 using YukkuriMovieMaker.Player.Video;
 
@@ -10,11 +9,9 @@ namespace YMMCustomEaseK
 {
     internal class EaseEditP : IVideoEffectProcessor
     {
-        BridgePlugin bridge = new BridgePlugin();
-        readonly private IGraphicsDevicesAndContext devices;
-        readonly EaseEdit item;
-        ID2D1Image? input;
-        double ReT = 0;
+        private readonly EaseEdit item;
+        private ID2D1Image? input;
+        private double ReT = 0;
 
         private static readonly IntPtr sharedLuaState;
         private static readonly object luaLock = new object();
@@ -31,47 +28,33 @@ namespace YMMCustomEaseK
         {
             this.item = item;
         }
-        
+
         [Obsolete]
         public DrawDescription Update(EffectDescription effectDescription)
         {
-            var frame = effectDescription.ItemPosition.Frame;
-            var length = effectDescription.ItemDuration.Frame;
-            var fps = effectDescription.FPS;
-            var layer = effectDescription.Layer;
-
+            var (frame, length, fps, layer) = (effectDescription.ItemPosition.Frame, effectDescription.ItemDuration.Frame, effectDescription.FPS, effectDescription.Layer);
             var drawDesc = effectDescription.DrawDescription;
-            var x = drawDesc.DrawPoint.X;
-            var y = drawDesc.DrawPoint.Y;
-            var zp = drawDesc.DrawPointZ;
-            var r = drawDesc.Rotation.Z;
-            var xr = drawDesc.Rotation.X;
-            var yr = drawDesc.Rotation.Y;
-            var zo = drawDesc.Zoom.X;
-            var cx = drawDesc.CenterPoint.X;
-            var cy = drawDesc.CenterPoint.Y;
+            var (x, y, zp, r, xr, yr, zo, cx, cy) = (drawDesc.DrawPoint.X, drawDesc.DrawPoint.Y, drawDesc.DrawPointZ, drawDesc.Rotation.Z, drawDesc.Rotation.X, drawDesc.Rotation.Y, drawDesc.Zoom.X, drawDesc.CenterPoint.X, drawDesc.CenterPoint.Y);
 
-            var file = item.File;
-            var subject = item.Enum;
-            var text = item.Text;
+            var (file, subject, text) = (item.File, item.Enum, item.Text);
 
             ReT = 0;
 
             string luaScriptPrefix = $@"
-            local _FRAME={frame} 
-            local _LENGTH={length} 
-            local _FPS={fps} 
-            local _LAYER={layer} 
-            local _X={x} 
-            local _Y={y} 
-            local _Z={zp}
-            local _R={r}
-            local _RX={xr}
-            local _RY={yr}
-            local _ZO={zo} 
-            local _CX={cx} 
-            local _CY={cy}
-            ";
+    local _FRAME={frame} 
+    local _LENGTH={length} 
+    local _FPS={fps} 
+    local _LAYER={layer} 
+    local _X={x} 
+    local _Y={y} 
+    local _Z={zp}
+    local _R={r}
+    local _RX={xr}
+    local _RY={yr}
+    local _ZO={zo} 
+    local _CX={cx} 
+    local _CY={cy}
+    ";
 
             try { text = File.ReadAllText(file); } catch { }
 
@@ -79,38 +62,17 @@ namespace YMMCustomEaseK
 
             lock (luaLock)
             {
-                try
-                {
-                    if (LuaJIT.luaL_loadstring(sharedLuaState, luaScript) == 0 && LuaJIT.lua_pcall(sharedLuaState, 0, 1, 0) == 0)
-                    {
-                        int top = LuaJIT.lua_gettop(sharedLuaState);
-
-                        if (top == 1)
-                        {
-                            ReT = LuaJIT.lua_tonumber(sharedLuaState, -1);
-                        }
-                    }
-                }
-                finally
-                {
-                    LuaJIT.lua_settop(sharedLuaState, 0);
-                }
+                ExecuteLuaScript(luaScript);
             }
 
             return subject switch
             {
                 EaseEdit.Subject.X => new DrawDescription(
                     drawDesc,
-                    draw: new System.Numerics.Vector2(
-                        drawDesc.DrawPoint.X + Convert.ToSingle(ReT),
-                        drawDesc.DrawPoint.Y
-                    )),
+                    draw: new System.Numerics.Vector2(x + Convert.ToSingle(ReT), y)),
                 EaseEdit.Subject.Y => new DrawDescription(
                     drawDesc,
-                    draw: new System.Numerics.Vector2(
-                        drawDesc.DrawPoint.X,
-                        drawDesc.DrawPoint.Y + Convert.ToSingle(ReT)
-                    )),
+                    draw: new System.Numerics.Vector2(x, y + Convert.ToSingle(ReT))),
                 EaseEdit.Subject.ZX => new DrawDescription(
                     drawDesc,
                     draw: new System.Numerics.Vector3(
@@ -118,43 +80,38 @@ namespace YMMCustomEaseK
                         )),
                 EaseEdit.Subject.R => new DrawDescription(
                     drawDesc,
-                    rotation: new System.Numerics.Vector3(
-                        drawDesc.Rotation.X,
-                        drawDesc.Rotation.Y,
-                        drawDesc.Rotation.Z + Convert.ToSingle(ReT)
-                    )),
+                    rotation: new System.Numerics.Vector3(xr, yr, r + Convert.ToSingle(ReT))),
                 EaseEdit.Subject.Z => new DrawDescription(
                     drawDesc,
-                    zoom: new System.Numerics.Vector2(
-                        drawDesc.Zoom.X + Convert.ToSingle(ReT)
-                    )),
+                    zoom: new System.Numerics.Vector2(zo + Convert.ToSingle(ReT), drawDesc.Zoom.Y)),
                 EaseEdit.Subject.XR => new DrawDescription(
                     drawDesc,
-                    rotation: new System.Numerics.Vector3(
-                        drawDesc.Rotation.X + Convert.ToSingle(ReT),
-                        drawDesc.Rotation.Y,
-                        drawDesc.Rotation.Z
-                    )),
+                    rotation: new System.Numerics.Vector3(xr + Convert.ToSingle(ReT), yr, r)),
                 EaseEdit.Subject.YR => new DrawDescription(
                     drawDesc,
-                    rotation: new System.Numerics.Vector3(
-                        drawDesc.Rotation.X,
-                        drawDesc.Rotation.Y + Convert.ToSingle(ReT),
-                        drawDesc.Rotation.Z
-                    )),
+                    rotation: new System.Numerics.Vector3(xr, yr + Convert.ToSingle(ReT), r)),
                 _ => drawDesc,
             };
         }
 
-        public void ClearInput() => input = null;
-
-        public void SetInput(ID2D1Image input) => this.input = input;
-
-        public void Dispose()
+        private void ExecuteLuaScript(string luaScript)
         {
+            if (LuaJIT.luaL_loadstring(sharedLuaState, luaScript) == 0 && LuaJIT.lua_pcall(sharedLuaState, 0, 1, 0) == 0)
+            {
+                int top = LuaJIT.lua_gettop(sharedLuaState);
+                if (top == 1)
+                {
+                    ReT = LuaJIT.lua_tonumber(sharedLuaState, -1);
+                }
+            }
+            LuaJIT.lua_settop(sharedLuaState, 0);
         }
 
-        class LuaJIT
+        public void ClearInput() => input = null;
+        public void SetInput(ID2D1Image input) => this.input = input;
+        public void Dispose() { }
+
+        private static class LuaJIT
         {
             private const string Lua51Dll = "lua51.dll";
 
@@ -163,15 +120,6 @@ namespace YMMCustomEaseK
 
             [DllImport(Lua51Dll, CallingConvention = CallingConvention.Cdecl)]
             public static extern void luaL_openlibs(IntPtr luaState);
-
-            [DllImport(Lua51Dll, CallingConvention = CallingConvention.Cdecl)]
-            public static extern void lua_close(IntPtr luaState);
-
-            [DllImport(Lua51Dll, CallingConvention = CallingConvention.Cdecl)]
-            public static extern void lua_pushcclosure(IntPtr luaState, LuaCSFunction function, int n);
-
-            [DllImport(Lua51Dll, CallingConvention = CallingConvention.Cdecl)]
-            public static extern void lua_setglobal(IntPtr luaState, string name);
 
             [DllImport(Lua51Dll, CallingConvention = CallingConvention.Cdecl)]
             public static extern int luaL_loadstring(IntPtr luaState, string chunk);
@@ -187,8 +135,6 @@ namespace YMMCustomEaseK
 
             [DllImport(Lua51Dll, CallingConvention = CallingConvention.Cdecl)]
             public static extern void lua_settop(IntPtr luaState, int index);
-
-            public delegate int LuaCSFunction(IntPtr luaState);
         }
     }
 }
